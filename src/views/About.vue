f3244949951d4242c1d01f7d87c6a9417e0f6331
Headcrab <94139390+HeadcrabJ@users.noreply.github.com>
2022-01-31 18:33:20 +0500

    Init commit

---
+++ src/views/About.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-container fill-height>
+    <v-row align="center" justify="center">
+      <h1>About page</h1>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'About',
+  metaInfo: {
+    title: 'About'
+  }
+}
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-container fill-height>
+    <v-row align="center" justify="center">
+      <div class="d-flex align-center mr-2">
+        <v-img
+            alt="Module Logo"
+            class="shrink mr-2"
+            contain
+            src="../assets/module.png"
+            transition="scale-transition"
+            width="40" />
+        <h2>Module</h2>
+      </div>
+      <div class="pa-4 my-mt">
+        <h4>
+          Multifunctional bot with moderation, information and utilities commands.
+        </h4>
+      </div>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    name: 'Home',
+    metaInfo: {
+      title: 'Home',
+    }
+  }
+</script>
+
+<style>
+  .my-mt {
+    margin-top: 2px;
+  }
+</style>
